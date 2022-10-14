14c03acd366ec851e0529b25975926e03d080598
aspirine312 <aspirine312@gmail.com>
2022-10-14 17:49:03 +0800

    game

--- wordle.py
+++ wordle.py
@@ -1,6 +1,21 @@
+import random
+
 if __name__=="__main__":
     #User input
     user_input = input()
 
     #specify an answer
-    answer="apple"
+    f=open("words.txt","r")
+    dictionary=f.read().splitlines()
+    f.close()
+    answer = random.sample(dictionary, 1)[0]
+    print(answer)
+
+    #compare user input and output
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
